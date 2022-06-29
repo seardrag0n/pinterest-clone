68f57598730cd101552a1881f26a68129bf884bd
SangBeen Yim <seardragonz@gmail.com>
2022-06-30 00:41:14 +0900

    Create ProfileView and handle image file

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,7 +1,9 @@
-from accountapp.urls import app_name
+from django.urls import path
+
+from profileapp.views import ProfileCreateView
 
 app_name = 'profileapp'
 
 urlpatterns = [
-
+    path('create/', ProfileCreateView.as_view(), name='create'),
 ]
