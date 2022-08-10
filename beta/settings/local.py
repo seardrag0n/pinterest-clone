9da408e4c0e3e29b92f13d3b782d9bc56b67c38b
SangBeen Yim <seardragonz@gmail.com>
2022-08-10 11:15:39 +0900

    minor error fixes in settings

--- beta/settings/local.py
+++ beta/settings/local.py
@@ -3,10 +3,6 @@ from .base import *
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
